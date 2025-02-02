6bdc8e717e5de90739835aa2296b37c68f753081
Joe <joe.gloach@gmail.com>
2025-02-02 18:46:40 +0000

    Remember ID and try and retrieve it

--- server/src/client.rs
+++ server/src/client.rs
@@ -6,12 +6,16 @@ use std::{
     },
 };
 
-use common::{data::PlayerData, event::server};
+use common::{
+    data::PlayerData,
+    event::{client, server},
+};
+use futures::{SinkExt, StreamExt};
 use tokio::{
     net::{TcpListener, TcpStream},
     sync::mpsc,
 };
-use tracing::{info, trace};
+use tracing::{error, info, trace, warn};
 
 use crate::{
     channels::Connection,
@@ -40,7 +44,7 @@ pub async fn connect(
             while let Ok((stream, addr)) = listener.accept().await {
                 let accepting = enabled.load(Ordering::Relaxed);
                 if accepting {
-                    setup_client(stream, addr, &mut data, &channels, disconnects.clone()).await;
+                    try_connect(stream, addr, &mut data, &channels, disconnects.clone()).await;
                 } else {
                     trace!("not accepting, dropped {addr}");
                     drop(stream);
@@ -55,24 +59,14 @@ pub async fn connect(
 #[derive(Clone)]
 pub struct Disconnects(mpsc::Sender<(uuid::Uuid, CloseReason)>);
 
-pub fn disconnect(data: GameData, channels: Channels) -> (Disconnects, tokio::task::AbortHandle) {
+pub fn disconnect(channels: Channels) -> (Disconnects, tokio::task::AbortHandle) {
     let (tx, mut rx) = mpsc::channel(16);
 
     let task = tokio::spawn(async move {
         while let Some((id, reason)) = rx.recv().await {
             info!("client {id} has left");
-            let player_count = {
-                let mut data = data.lock();
-                data.remove_player(id);
-                data.player_count()
-            };
             channels.remove(id).await;
-            channels
-                .broadcast_event(server::Event::Left {
-                    uuid: id,
-                    player_count,
-                })
-                .await;
+            channels.broadcast_event(server::Event::Left { id }).await;
             // let subscribers know theres been a disconnection
             let _ = channels
                 .connections()
@@ -83,41 +77,81 @@ pub fn disconnect(data: GameData, channels: Channels) -> (Disconnects, tokio::ta
     (Disconnects(tx), task.abort_handle())
 }
 
-async fn setup_client(
+async fn try_connect(
     stream: TcpStream,
     addr: SocketAddr,
     data: &mut GameData,
     channels: &Channels,
     disconnects: Disconnects,
 ) {
-    info!("new connection from {addr}");
-    let player = PlayerData::new();
-    let player_id = player.id();
+    info!("connection from {addr}");
 
-    let player_count = {
-        let mut data = data.lock();
+    let mut connection = PlayerConn::from(stream);
 
-        data.add_player(player);
-        data.player_count()
+    // retrieve id of player
+    let Some(id) = id_handshake(data, &mut connection).await else {
+        return;
     };
 
+    // let other clients know someone has joined
+    trace!("sending join");
+    channels.broadcast_event(server::Event::Joined { id }).await;
+
+    // let subscribers know theres a new connection
+    let _ = channels.connections().send(Connection::Connect(id));
+
     // spawn a player task
-    let left = player::spawn(channels, player_id, PlayerConn::from(stream)).await;
+    let left = player::spawn(Arc::clone(data), channels, id, connection).await;
 
     // let the disconnect handler know
     tokio::spawn(async move {
         if let Ok(reason) = left.await {
-            let _ = disconnects.0.send((player_id, reason)).await;
+            let _ = disconnects.0.send((id, reason)).await;
         }
     });
 
-    // let everyone know someone has joined
+    // signal to the player that they can join the event loop
     channels
-        .broadcast_event(server::Event::Joined {
-            uuid: player_id,
-            player_count,
-        })
+        .send(player::Command::Event(server::Event::Enter), id)
         .await;
-    // let subscribers know theres a new connection
-    let _ = channels.connections().send(Connection::Connect(player_id));
+}
+
+async fn id_handshake(data: &mut GameData, connection: &mut PlayerConn) -> Option<uuid::Uuid> {
+    let Some(Ok(client::Event::Join(join))) = connection.read.next().await else {
+        warn!("connection refused as client never requested to join");
+        return None;
+    };
+
+    let id = retrieve_or_create_id(data, join);
+
+    // assign the player their id
+    if let Err(e) = connection.write.send(server::Event::AssignId { id }).await {
+        error!("failed to assign id to client: {e}");
+        return None;
+    }
+
+    Some(id)
+}
+
+fn retrieve_or_create_id(data: &mut GameData, join: client::Join) -> uuid::Uuid {
+    let create_new = || {
+        let player = PlayerData::new();
+        let id = player.id();
+
+        data.lock().try_add_player(player);
+
+        id
+    };
+
+    match join {
+        client::Join::New => create_new(),
+        client::Join::Existing(id) if !data.lock().exists(id) => {
+            warn!("client tried to connect with invalid id");
+            create_new()
+        }
+        client::Join::Existing(id) => {
+            debug_assert!(data.lock().exists(id));
+            id
+        }
+    }
 }
