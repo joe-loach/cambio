d9775cddae1d4ff386f52045f7a8dcd08a253993
Joe <joe.gloach@gmail.com>
2025-02-02 18:45:29 +0000

    Add Join and LobbyInfo events

--- common/src/event/client.rs
+++ common/src/event/client.rs
@@ -4,9 +4,17 @@ use crate::decisions::Decision;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum Event {
+    Join(Join),
+    GetLobbyInfo,
     Start,
     Snap,
     Decision(Decision),
     Continue,
     Leave,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum Join {
+    New,
+    Existing(uuid::Uuid),
 }
